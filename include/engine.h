783da1ca8abefb9ba30919b8054b5a368ac4a8e1
Louis Cianciullo <lccianciullo@gmail.com>
2023-03-30 16:30:40 +1030

    Removed OPENGL addiitons for now.
    Decided to just focus on the core raytracer for now and rmeove the
    interactive display part until later.

--- include/engine.h
+++ include/engine.h
@@ -1,7 +1,6 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include "display.h"
 #include "canvas.h"
 #include <stdbool.h>
 
--- src/engine.c
+++ src/engine.c
@@ -1,23 +1,19 @@
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "../include/engine.h"
 
 void engineInit(int width, int height) {
     printf("engineInit\n");
-    initDisplay();
+
     canvas = createCanvas(width, height);
 }
 
 void engineMainLoop() {
-    while (!shouldDisplayClose()) {
-        //raytrace(canvas)
-        // display(canvas)
-        glfwPollEvents();
-    }
+
 }
 
 
 void engineDestroy() {
     destroyCanvas(canvas);
-    destroyDisplay();
 }

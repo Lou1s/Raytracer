0b1b5a7e1611f964df5e0cbf8cb0adfa09cce8a1
Louis Cianciullo <lccianciullo@gmail.com>
2023-05-02 22:06:46 +0930

    Extended Tuple capability
    - Added a point and vector function that return tuples with w values of
      1.0 and 0.0 respectively.
    - Added an equality function for determining if two tuples are euqal.

--- include/tuple.h
+++ include/tuple.h
@@ -1,7 +1,25 @@
 #ifndef TUPLE_H
 #define TUPLE_H
+
+#include <stdbool.h>
+
 typedef struct Tuple {
     float x,y,z,w;
  } Tuple;
+
+static inline Tuple point(const float x, const float y, const float z) {
+    Tuple p = {x, y, z, 1.0f};
+    return p;
+}
+
+static inline Tuple vector(const float x, const float y, const float z) {
+    Tuple v = {x, y, z, 0.0f};
+    return v;
+}
+
+static inline bool tupleEqual(Tuple a, Tuple b) {
+    return a.x == b.x && a.y == b.y && a.z == b.z && a.w == b.w;
+}
+
 
 #endif
--- tests/tupleTest.c
+++ tests/tupleTest.c
@@ -26,10 +26,37 @@ static void tupleVectorInitTest(void **state) {
 
 }
 
+static void pointInitTest(void **state) {
+    Tuple p = point(3.1, 2.0, 4.0);
+    Tuple pp = {3.1, 2.0, 4.0, 1.0};
+    assert_float_equal(p.w, 1.0, EPSILON);
+    assert_true(tupleEqual(p, pp));
+}
+
+static void vectorInitTest(void **state) {
+    Tuple v = vector(0.1, 0.0, 101.1);
+    Tuple vv = {0.1, 0.0, 101.1, 0.0};
+    assert_float_equal(v.w, 0.0, EPSILON);
+    assert_true(tupleEqual(v, vv));
+}
+
+static void tupleEqualTest(void **state) {
+    Tuple a = {9.0, 5.45, 6.1, 1.0};
+    Tuple b = {0.3, 5.45, 6.1, 1.0};
+    assert_false(tupleEqual(a, b));
+    b.x = 9.0;
+    assert_true(tupleEqual(a, b));
+    a.y = a.z = a.w = 0.0;
+    assert_false(tupleEqual(a, b));
+    }
+
 int main() {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(tuplePointInitTest),
-        cmocka_unit_test(tupleVectorInitTest)
+        cmocka_unit_test(tupleVectorInitTest),
+        cmocka_unit_test(pointInitTest),
+        cmocka_unit_test(vectorInitTest),
+        cmocka_unit_test(tupleEqualTest)
     };
     return cmocka_run_group_tests_name("TupleTest", tests, NULL, NULL);
 }

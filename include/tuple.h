48d6337723d006b58c43aa3b3ff24e898070dff1
Louis Cianciullo <lccianciullo@gmail.com>
2023-05-02 22:21:04 +0930

    Fixed Tuple Equality Function
    Added an EPSILON constant that is used to (more) accurately check the
    equality of floats in the tuple equality function.

--- include/tuple.h
+++ include/tuple.h
@@ -2,6 +2,9 @@
 #define TUPLE_H
 
 #include <stdbool.h>
+#include <math.h>
+
+#define EPSILON 0.000001f
 
 typedef struct Tuple {
     float x,y,z,w;
@@ -17,8 +20,9 @@ static inline Tuple vector(const float x, const float y, const float z) {
     return v;
 }
 
-static inline bool tupleEqual(Tuple a, Tuple b) {
-    return a.x == b.x && a.y == b.y && a.z == b.z && a.w == b.w;
+static inline bool tuplesEqual(Tuple a, Tuple b) {
+    return fabs(a.x - b.x) <= EPSILON && fabs(a.y - b.y) <= EPSILON && fabs(a.z - b.z) <= EPSILON && 
+           fabs(a.w - b.w) <= EPSILON;
 }
 
 
--- tests/tupleTest.c
+++ tests/tupleTest.c
@@ -6,8 +6,6 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-#define EPSILON 0.000001f
-
 static void tuplePointInitTest(void **state) {
     Tuple point = {4.3, -4.2, 3.1, 1.0};
     assert_float_equal(point.x, 4.3, EPSILON);
@@ -30,24 +28,25 @@ static void pointInitTest(void **state) {
     Tuple p = point(3.1, 2.0, 4.0);
     Tuple pp = {3.1, 2.0, 4.0, 1.0};
     assert_float_equal(p.w, 1.0, EPSILON);
-    assert_true(tupleEqual(p, pp));
+    assert_true(tuplesEqual(p, pp));
 }
 
 static void vectorInitTest(void **state) {
     Tuple v = vector(0.1, 0.0, 101.1);
     Tuple vv = {0.1, 0.0, 101.1, 0.0};
     assert_float_equal(v.w, 0.0, EPSILON);
-    assert_true(tupleEqual(v, vv));
+    assert_true(tuplesEqual(v, vv));
 }
 
-static void tupleEqualTest(void **state) {
+static void tuplesEqualTest(void **state) {
     Tuple a = {9.0, 5.45, 6.1, 1.0};
     Tuple b = {0.3, 5.45, 6.1, 1.0};
-    assert_false(tupleEqual(a, b));
+    assert_false(tuplesEqual(a, b));
     b.x = 9.0;
-    assert_true(tupleEqual(a, b));
+    assert_true(tuplesEqual(a, b));
     a.y = a.z = a.w = 0.0;
-    assert_false(tupleEqual(a, b));
+    assert_false(tuplesEqual(a, b));
+
     }
 
 int main() {
@@ -56,7 +55,7 @@ int main() {
         cmocka_unit_test(tupleVectorInitTest),
         cmocka_unit_test(pointInitTest),
         cmocka_unit_test(vectorInitTest),
-        cmocka_unit_test(tupleEqualTest)
+        cmocka_unit_test(tuplesEqualTest)
     };
     return cmocka_run_group_tests_name("TupleTest", tests, NULL, NULL);
 }

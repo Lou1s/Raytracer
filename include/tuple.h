990be7fe27f8be3a2c230c198e1289d541141e83
Louis Cianciullo <lccianciullo@gmail.com>
2023-05-03 18:06:52 +0930

    Added Basic Tuple Operations
    Added negation, addition and subtraction functions (plus tests) for the
    tuples.

--- include/tuple.h
+++ include/tuple.h
@@ -20,9 +20,22 @@ static inline Tuple vector(const float x, const float y, const float z) {
     return v;
 }
 
-static inline bool tuplesEqual(Tuple a, Tuple b) {
+static inline bool tuplesEqual(const Tuple a, const Tuple b) {
     return fabs(a.x - b.x) <= EPSILON && fabs(a.y - b.y) <= EPSILON && fabs(a.z - b.z) <= EPSILON && 
            fabs(a.w - b.w) <= EPSILON;
+}
+
+static inline Tuple addTuples(const Tuple a, const Tuple b) {
+    return (Tuple) {a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w};
+}
+
+static inline Tuple negateTuple(const Tuple t) {
+    return (Tuple){-t.x, -t.y, -t.z, -t.w};
+ 
+}
+
+static inline Tuple subtractTuples(const Tuple a, const Tuple b) {
+    return addTuples(a, negateTuple(b));
 }
 
 
--- tests/tupleTest.c
+++ tests/tupleTest.c
@@ -49,13 +49,48 @@ static void tuplesEqualTest(void **state) {
 
     }
 
+static void negateTupleTest(void **state) {
+        Tuple p = point(1.1, 2.3, -4.0);
+        Tuple pp = negateTuple(p);
+        assert_float_equal(pp.x, -1.1, EPSILON);
+        assert_float_equal(pp.y, -2.3, EPSILON);
+        assert_float_equal(pp.z, 4.0, EPSILON);
+        assert_float_equal(pp.w, -1.0, EPSILON);
+
+        Tuple v = vector(-1.1, -2.3, 4.0);
+        Tuple vv = negateTuple(v);
+        assert_float_equal(vv.x, 1.1, EPSILON);
+        assert_float_equal(vv.y, 2.3, EPSILON);
+        assert_float_equal(vv.z, -4.0, EPSILON);
+        assert_float_equal(vv.w, 0.0, EPSILON);
+
+    }
+static void addTupleTest(void **state) {
+    Tuple v = vector(1.0, 2.4, -5.0);
+    Tuple p = point(3.1, -2.4, -20.0);
+    Tuple sum = addTuples(v, p);
+    Tuple expected = {1.0 + 3.1, 2.4 - 2.4, -5.0 - 20.0, 0.0 + 1.0};
+    assert_true(tuplesEqual(expected, sum));
+}
+
+static void subtractTupleTest(void **state) {
+    Tuple v = vector(1.0, 2.4, -5.0);
+    Tuple p = point(3.1, -2.4, -20.0);
+    Tuple diff = subtractTuples(v, p);
+    Tuple expected = {1.0 - 3.1, 2.4 + 2.4, -5.0 + 20.0, 0.0 - 1.0};
+    assert_true(tuplesEqual(expected, diff));
+}
+
 int main() {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(tuplePointInitTest),
         cmocka_unit_test(tupleVectorInitTest),
         cmocka_unit_test(pointInitTest),
         cmocka_unit_test(vectorInitTest),
-        cmocka_unit_test(tuplesEqualTest)
+        cmocka_unit_test(tuplesEqualTest),
+        cmocka_unit_test(negateTupleTest),
+        cmocka_unit_test(addTupleTest),
+        cmocka_unit_test(subtractTupleTest)
     };
     return cmocka_run_group_tests_name("TupleTest", tests, NULL, NULL);
 }

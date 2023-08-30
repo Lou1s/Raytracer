30ec51baff8eb7ae0683626d6f6a8724a1cde87b
Louis Cianciullo <lccianciullo@gmail.com>
2023-08-30 22:31:07 +0930

    Tuple Documentation
    Added some brief documentation in the header file file for the Tuples.
    Don't really feel the need to go into greater detail as the
    implementation is pretty simple.

--- include/tuple.h
+++ include/tuple.h
@@ -6,15 +6,22 @@
 
 #define EPSILON 0.000001f
 
+/* Generic Tuple data type, used as a basis for both vectors and points*/
 typedef struct Tuple {
     float x,y,z,w;
  } Tuple;
 
+/* Constructors for vectors (Tuple) {x, y, z, 0.0} and points (Tuple) {x, y, z, 1.0}*/
 Tuple point(const float x, const float y, const float z);
 Tuple vector(const float x, const float y, const float z);
 
+/* Helper function(s) for debugging*/
 void printTuple(const Tuple t);
 
+/* General operations performed on two tuples tuples, all operations return a new completely new 
+tuple. There is no in place operators. Note that functions will usually describe what kind of Tuple
+should be passed in as input, i.e. dotprodVectors is only expecting vectors, whilst multTupleScalar
+takes in a generic tuple e.g. a point or a vector. */
 bool tuplesEqual(const Tuple a, const Tuple b);
 Tuple addTuples(const Tuple a, const Tuple b);
 Tuple negateTuple(const Tuple t);
@@ -24,6 +31,9 @@ Tuple divideTupleScalar (const Tuple t, float s);
 Tuple crossProductVectors(const Tuple a, const Tuple b);
 float dotProdVectors(const Tuple a, Tuple b);
 
+/* Vector magnitute and normalise functions. Operates under the premise that the input is a vector 
+i.e. the w component of the tuple is completely ignored. vectorNormalise also returns a vector, 
+deliberately setting w to 0.0. */
 float vectorMagnitude(const Tuple v);
 Tuple vectorNormalise(const Tuple v);
 
--- src/engine.c
+++ src/engine.c
@@ -1,6 +1,5 @@
 #include "../include/engine.h"
 
-
 #include <stdbool.h>
 #include <stdio.h>
 

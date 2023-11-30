d1dc1878e172b6e98430706842b2e694bd3977db
Louis Cianciullo <lccianciullo@gmail.com>
2023-11-30 17:12:28 +1030

    Created floatEquals macro.

--- include/constants.h
+++ include/constants.h
@@ -1,8 +1,9 @@
 #ifndef COSNTANTS_H
 #define COSNTANTS_H
 
+#include <math.h>
+
 /* Storing constants and other common things here */
-
 #define EPSILON 0.000001f
-
+#define floatEqual(f1, f2) (fabs(f1 - f2) <= EPSILON)
 #endif
